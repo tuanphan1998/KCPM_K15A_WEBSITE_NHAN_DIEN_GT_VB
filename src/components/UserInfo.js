c0912b025cfd9706a58e47744186436c5b2d2491
TUanphan nam <phantuanw.o.t@gmail.com>
2020-02-17 15:23:50 +0700

    s

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -76,32 +76,32 @@ class UserInfo extends Component {
 									<header className="App-header">
 										<div className="container mt-5 mb-4">
 												<div className="row">
-												<div className="col-4">
-													<div className="list-group" id="list-tab" role="tablist">
-														<a  className="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Front card</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Back card</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list2" data-toggle="list" href="#list-profile2" role="tab" aria-controls="profile">Back up Card</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list3" data-toggle="list" href="#list-profile3" role="tab" aria-controls="profile">InFo Edit Card</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list4" data-toggle="list" href="#list-profile4" role="tab" aria-controls="profile">Language</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list7" data-toggle="list" href="#list-profile7" role="tab" aria-controls="profile">Language Data Show</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list5" data-toggle="list" href="#list-profile5" role="tab" aria-controls="profile">Language Data del</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list6" data-toggle="list" href="#list-profile6" role="tab" aria-controls="profile">Put card no AI</a>
-														<a className="list-group-item list-group-item-action" id="list-profile-list8" data-toggle="list" href="#list-profile8" role="tab" aria-controls="profile">Infos system</a>
-													</div>
-												</div>
-												<div className="col-8">
-													<div className="tab-content" id="nav-tabContent">
-														<div className="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"> Mặt trước cung cấp đủ thông tin cho người dùng bao gồm tự động nhận diện ảnh face , thông tin mã <code>cmnd or số cccd</code> địa chỉ quê quán và ngày sinh <a href="/mat-truoc">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Mặt sau cung cấp đủ thông tin cho người dùng bao gồm tự động nhận diện văn bản khu vực tôn giáo , dân tộc , nơi cấp <a href="/mat-sau">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile2" role="tabpanel" aria-labelledby="list-profile-list2">Đưa ra danh sách user thống kê lưu trữ mọi thông tin đã cấp quyền quét <a href="/danh-sach">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile3" role="tabpanel" aria-labelledby="list-profile-list3">Đưa ra các sự lựa chọn về sửa đổi dữ liệu user và thống kê tỉ lệ các thay đổi đã thiết lập<a href="/thong-ke">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile4" role="tabpanel" aria-labelledby="list-profile-list4">Đưa ra công cụ chuyển hóa văn bản thành giọng nói đảm bảo chất lượng âm thanh tốt mang lại một trải ngiệm tốt <a href="/giong-noi">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile7" role="tabpanel" aria-labelledby="list-profile-list7"> Lưu trữ toàn tệp âm thanh và hình ảnh tài liệu của nhà bạn :))<a href="/hien-thi-danh-sach">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile5" role="tabpanel" aria-labelledby="list-profile-list5"> Lưu trữ toàn bộ tệp âm thanh và các thao tác xóa văn bản hành chính online của bạn <a href="/du-lieu-giong-noi">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile6" role="tabpanel" aria-labelledby="list-profile-list6"> Update data không sử dụng hình ảnh và công nghệ trí tuệ nhân tạo <a href="/du-lieu-update-no-ai">Ấn vào đây</a></div>
-														<div className="tab-pane fade" id="list-profile8" role="tabpanel" aria-labelledby="list-profile-list8"> View info system tổng hợp lại toàn bộ thông tin nhà phát triển , tổng số lần gọi <a href="/thong-tin-he-thong">Ấn vào đây</a></div>
-													</div>
-												</div>
+																<div className="col-4">
+																	<div className="list-group" id="list-tab" role="tablist">
+																		<a  className="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Front card</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Back card</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list2" data-toggle="list" href="#list-profile2" role="tab" aria-controls="profile">Back up Card</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list3" data-toggle="list" href="#list-profile3" role="tab" aria-controls="profile">InFo Edit Card</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list4" data-toggle="list" href="#list-profile4" role="tab" aria-controls="profile">Language</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list7" data-toggle="list" href="#list-profile7" role="tab" aria-controls="profile">Language Data Show</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list5" data-toggle="list" href="#list-profile5" role="tab" aria-controls="profile">Language Data del</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list6" data-toggle="list" href="#list-profile6" role="tab" aria-controls="profile">Put card no AI</a>
+																		<a className="list-group-item list-group-item-action" id="list-profile-list8" data-toggle="list" href="#list-profile8" role="tab" aria-controls="profile">Infos system</a>
+																	</div>
+																</div>
+																<div className="col-8">
+																	<div className="tab-content" id="nav-tabContent">
+																		<div className="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"> Mặt trước cung cấp đủ thông tin cho người dùng bao gồm tự động nhận diện ảnh face , thông tin mã <code>cmnd or số cccd</code> địa chỉ quê quán và ngày sinh <a href="/mat-truoc">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Mặt sau cung cấp đủ thông tin cho người dùng bao gồm tự động nhận diện văn bản khu vực tôn giáo , dân tộc , nơi cấp <a href="/mat-sau">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile2" role="tabpanel" aria-labelledby="list-profile-list2">Đưa ra danh sách user thống kê lưu trữ mọi thông tin đã cấp quyền quét <a href="/danh-sach">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile3" role="tabpanel" aria-labelledby="list-profile-list3">Đưa ra các sự lựa chọn về sửa đổi dữ liệu user và thống kê tỉ lệ các thay đổi đã thiết lập<a href="/thong-ke">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile4" role="tabpanel" aria-labelledby="list-profile-list4">Đưa ra công cụ chuyển hóa văn bản thành giọng nói đảm bảo chất lượng âm thanh tốt mang lại một trải ngiệm tốt <a href="/giong-noi">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile7" role="tabpanel" aria-labelledby="list-profile-list7"> Lưu trữ toàn tệp âm thanh và hình ảnh tài liệu của nhà bạn :))<a href="/hien-thi-danh-sach">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile5" role="tabpanel" aria-labelledby="list-profile-list5"> Lưu trữ toàn bộ tệp âm thanh và các thao tác xóa văn bản hành chính online của bạn <a href="/du-lieu-giong-noi">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile6" role="tabpanel" aria-labelledby="list-profile-list6"> Update data không sử dụng hình ảnh và công nghệ trí tuệ nhân tạo <a href="/du-lieu-update-no-ai">Ấn vào đây</a></div>
+																		<div className="tab-pane fade" id="list-profile8" role="tabpanel" aria-labelledby="list-profile-list8"> View info system tổng hợp lại toàn bộ thông tin nhà phát triển , tổng số lần gọi <a href="/thong-tin-he-thong">Ấn vào đây</a></div>
+																	</div>
+																</div>
 											</div>
 											<DieuHuongUrl/>
 										</div>
