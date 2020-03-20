855708faf07d247cdd400bf377db722d61624992
TUanphan nam <phantuanw.o.t@gmail.com>
2020-03-20 09:04:40 +0700

    s

--- src/views/VideoInput.js
+++ src/views/VideoInput.js
@@ -51,7 +51,7 @@ class VideoInput extends Component {
   startCapture = () => {
     this.interval = setInterval(() => {
       this.capture();
-    }, 1500);
+    }, 500);
   };
 
   componentWillUnmount() {
